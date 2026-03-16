931db4c2bd00a61d3956cf924e56149781bced73
Marco Fröhlich <marco.froehlich@student.uibk.ac.at>
2026-03-16 12:21:19 +0100

    making paths absolute

--- tools/load_generator/exec_with_workstation_heavy.sh
+++ tools/load_generator/exec_with_workstation_heavy.sh
@@ -1,10 +1,10 @@
 killall loadgen &> /dev/null
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
-../build/loadgen mc3 workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
+/home/cb76/cb761228/perf-oriented-dev/tools/build/loadgen mc3 /home/cb76/cb761228/perf-oriented-dev/tools/load_generator/workstation/sys_load_profile_workstation_excerpt.txt &> /dev/null &
 #time -p nice -n 100 $1
 nice -n 1000 $1
 killall loadgen &> /dev/null
